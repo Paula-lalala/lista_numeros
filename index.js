5fc40501c1b8dc81dd4b8a59cd24d1c2b34f71fe
DianaCardozo <125745016+DianaCardozo@users.noreply.github.com>
2024-03-15 13:19:05 -0500

    Update index.js
    "Se creó la función para recorrer el array y ya se puede ver en el evento al oprimir el boton 'ver', falta especificar las otras opciones y que se muestre dicha lista"

--- index.js
+++ index.js
@@ -16,9 +16,30 @@ document.getElementById('verBtn').addEventListener('click', function() {
     if (numeroInput < 1 || numeroInput >200 || isNaN(numeroInput)) {
         alert("Recuerde que la lista solo recorre del 1-200, si puso un número inferior a 1 o cercano al 200 según su opción no se mostrará la lista al estar al final de esta");
         return;
-    }
+    } 
+     funcion_ver(opcionSeleccionada, numeroInput, resultadoList);
 });
 
+
+function funcion_ver(opcionSeleccionada, numeroInput, resultadoList) {
+    for (var i = 1; i <= 200; i++) {
+        if ((opcionSeleccionada === 'pares' && i % 2 === 0) ||
+            (opcionSeleccionada === 'impares' && i % 2 !== 0)) {
+            var li = document.createElement('li');
+            li.textContent = i;
+            resultadoList.appendChild(li);
+             } else if ((opcionSeleccionada === 'NMpares' && i % 2 === 0 && i > numeroInput && numeroInput<=199) ||
+            (opcionSeleccionada === 'NMimpares' && i % 2 !== 0 && i > numeroInput && numeroInput<=198)) {
+            var li = document.createElement('li');
+            li.textContent = i;
+            resultadoList.appendChild(li);
+        } else if((opcionSeleccionada === 'NMpares' && numeroInput>199) || 
+        (opcionSeleccionada === 'NMimpares' && numeroInput>198)){
+            alert("Recuerde que la lista solo recorre del 1-200, si puso un número inferior a 1 o cercano al 200 según su opción no se mostrará la lista al estar al final de esta");
+        return;
+        }
+    }
+}
 document.getElementById('opciones').addEventListener('click', function() {
     var resultadoList = document.getElementById('resultado');
     resultadoList.innerHTML = '';
