902f90832c4a7bdac86d0e67b4012855e20278bf
unknown <paortegon@uniboyaca.edu.co>
2024-03-15 11:17:36 -0500

    Se modificó el archivo index.html para establecer una sección en donde se muestre la lista de números al eleguir una opción, en el archivo js se agrego las funciones al elegir una de las 4 opciones, al elegir las últimas 2 aparecera la sección oculta y según los parametros se procedera a guardar el dato en una variable o se mostrará un mensaje de alerta

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ document.getElementById('opciones').addEventListener('change', function() {
     }
 });
 
+document.getElementById('verBtn').addEventListener('click', function() {
+    var resultadoList = document.getElementById('resultado');
+    resultadoList.innerHTML = '';
+    var opcionSeleccionada = document.getElementById('opciones').value;
+    var numeroInput = parseInt(document.getElementById('numero').value);
+
+    if (numeroInput < 1 || numeroInput >200 || isNaN(numeroInput)) {
+        alert("Recuerde que la lista solo recorre del 1-200, si puso un número inferior a 1 o cercano al 200 según su opción no se mostrará la lista al estar al final de esta");
+        return;
+    }
+});
+
+document.getElementById('opciones').addEventListener('click', function() {
+    var resultadoList = document.getElementById('resultado');
+    resultadoList.innerHTML = '';
+    var opcionSeleccionada = document.getElementById('opciones').value;
+    var numeroInput = parseInt(document.getElementById('numero').value);
+});
 
 
 
